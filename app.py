eb40105855cbca4527f8981df71b8498b00e0fc0
parker-g <94263482+parker-g@users.noreply.github.com>
2022-09-24 19:37:42 -0400

    first flask commit
    installed flask to venv, began creating a flask app to serve front end. learning about flask now and the proper way to use it

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, redirect, url_for, render_template
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+
+
+
+@app.route("/<name>/") # can pass a string from URL as an argument through <name>. this name is then used in this function
+def user(name):
+    return render_template(
+    'index.html',
+     content=name,
+     )
+@app.route('/admin/')
+def admin():
+    return redirect(url_for('user', name= 'Admin!')) # redirect to user function and provides an argument for 'name' parameter
+
+if __name__ =='__main__':
+    app.run()
+
